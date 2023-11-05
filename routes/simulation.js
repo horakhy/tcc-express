b85d8c503e7fa55fdf54e2bbbaa8bbc96cdf1a4f
José Henrique Ivanchechen <jose.henrique.ivan@gmail.com>
2023-11-04 21:22:59 -0300

    adding protobuf and json

--- routes/simulation.js
+++ routes/simulation.js
@@ -1,6 +1,10 @@
-import express from 'express'
+import express, { json } from 'express'
+import protobuf from 'protobufjs';
 
 const router = express.Router();
+
+const root = await protobuf.load('person.proto');
+const Person = root.lookupType('Person');
 
 router.get('/harmonic-progression', (req, res) => {
     const n = req.query.n;
@@ -14,10 +18,16 @@ router.get('/harmonic-progression', (req, res) => {
     res.json(sum);
 })
 
-router.get('/json', (req, res) => {
-    res.json({
-        message: 'Hello World!'
-    });
+router.post('/json', express.json(), (req, res) => {
+    const person = req.body;
+    res.send(person)
+})
+
+router.post('/protobuf', express.text({ type: 'application/protobuf' }), (req, res) => {
+    const personRequest = Person.decode(Buffer.from(req.body));
+
+    const response = Person.encode(personRequest).finish();
+    res.send(response);
 })
 
 export default router;
