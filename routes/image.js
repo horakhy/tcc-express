930a322dfac69d25aa2b91ed478c2c4d46bfc68d
José Henrique Ivanchechen <jose.henrique.ivan@gmail.com>
2024-03-24 20:06:50 -0300

    removing unneeded endpoints

--- routes/image.js
+++ routes/image.js
@@ -3,21 +3,6 @@ import { readFileImage, saveImage } from '../controller/image-controller.js';
 
 const router = express.Router();
 
-router.get('/load-small-image', (req, res) => {
-    const image = readFileImage('static/small-image.png');
-
-    res.set('Content-Type', 'image/png');
-    res.send(image);
-})
-
-router.get('/load-big-image', (req, res) => {
-    const image = readFileImage('static/big-image.png');
-
-    res.set('Content-Type', 'image/png');
-    res.send(image);
-
-})
-
 router.post('/save-big-image', (req, res) => {
     saveImage(req.body);
 
